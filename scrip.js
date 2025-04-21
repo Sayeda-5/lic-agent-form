46f0ff313f46dda04b9d398013d7cf86fde5b391
sayeda-5 <sidrasyed199930@gmail.com>
2025-04-22 00:22:08 +0530

    Initial LIC Form

---
+++ scrip.js
@@ -0,0 +1,8 @@
+
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("LIC Policy Enquiry Form Ready");
+    });
+ document.getElementById("submitBtn").addEventListener("click", function () {
+    console.log("Form submitted!");
+    });
+            
